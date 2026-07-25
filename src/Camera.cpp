1a6be2ae93ce3f16eb9c90e976a590c1dec9fa17
jonathan <jonathhu@stud.ntnu.no>
2026-07-25 22:01:39 +0200

    Fix implicit type conversion

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,7 +1,7 @@
 #include "../include/Camera.hpp"
 
 Vec2i Camera::ProjectTo2D(const Vec3f& v) const {
-    int x_proj = (v.x + 1.0) * width / 2;
-    int y_proj = (v.y + 1.0) * height / 2;
-    return Vec2i(x_proj, y_proj);
+    auto x_proj = static_cast<int>((v.x + 1.0) * width / 2);
+    auto y_proj = static_cast<int>((v.y + 1.0) * height / 2);
+    return Vec2i{x_proj, y_proj};
 }
