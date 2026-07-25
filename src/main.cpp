c47ca24bde2540f288e15261968d345bb0357f1f
jonathan <jonathhu@stud.ntnu.no>
2026-07-25 22:33:36 +0200

    Allow for multiple model cli inputs

--- src/main.cpp
+++ src/main.cpp
@@ -12,22 +12,29 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    const char* srcPath = argv[1];
+    std::vector<Mesh> meshes;
+
+    for (int i = 1; i < argc - 1; i++) {
+        const char* path = argv[i];
+
+        if (std::filesystem::is_directory(path)) {
+            for (auto const& dirEntry : std::filesystem::directory_iterator(path)) {
+                if (dirEntry.path().extension() != ".obj") {
+                    continue;
+                }
+                Mesh mesh(dirEntry);
+                meshes.push_back(mesh);
+            }
+            break;
+        }
+
+        Mesh mesh(path);
+        meshes.push_back(mesh);
+    }
+
     const char* dstPath = argv[argc - 1];
 
     Camera camera(width, height);
-
-    std::vector<Mesh> meshes;
-
-    if (std::filesystem::is_directory(srcPath)) {
-        for (auto const& dirEntry : std::filesystem::directory_iterator(srcPath)) {
-            if (dirEntry.path().extension() != ".obj") {
-                continue;
-            }
-            Mesh mesh(dirEntry);
-            meshes.push_back(mesh);
-        }
-    }
 
     Image img(camera);
 
