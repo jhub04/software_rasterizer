a8b5acfff67fb4c2cafaf19b18009345f2eafa26
jonathan <jonathhu@stud.ntnu.no>
2026-07-25 15:51:39 +0200

    Add another model

---
+++ Mesh.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jonathan Hübertz on 25/07/2026.
+//
+
+#include "Mesh.hpp"
---
+++ Mesh.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Jonathan Hübertz on 25/07/2026.
+//
+
+#ifndef MESH_HPP
+#define MESH_HPP
+
+
+
+class mesh {
+
+};
+
+
+
+#endif //MESH_HPP
