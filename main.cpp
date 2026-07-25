bd2b0529f5e6dfb34c1c9cc69a97330bf8c161a1
jonathan <jonathhu@stud.ntnu.no>
2026-07-25 18:26:08 +0200

    Draw vertices and faces

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ constexpr Pixel green = {0, 255, 0};
 constexpr Pixel blue = {255, 0, 0};
 
 int main(int argc, char* argv[]) {
-    constexpr uint16_t width = 256;
-    constexpr uint16_t height = 256;
+    constexpr uint16_t width = 800;
+    constexpr uint16_t height = 800;
 
     if (argc != 2) {
         std::cerr << "Only one argument is supported!" << std::endl;
@@ -26,6 +26,22 @@ int main(int argc, char* argv[]) {
     for (Vec3& v : mesh.GetVertices()) {
         std::tuple proj = img.ProjectTo2D(v);
         img.SetPixel(std::get<0>(proj), std::get<1>(proj), red);
+    }
+
+    for (Face& f : mesh.GetFaces()) {
+        std::vector<Vec3>& vertices= mesh.GetVertices();
+
+        Vec3 v1 = vertices[f.vIndices[0]];
+        Vec3 v2 = vertices[f.vIndices[1]];
+        Vec3 v3 = vertices[f.vIndices[2]];
+
+        std::tuple projV1 = img.ProjectTo2D(v1);
+        std::tuple projV2 = img.ProjectTo2D(v2);
+        std::tuple projV3 = img.ProjectTo2D(v3);
+
+        img.DrawLine(std::get<0>(projV1), std::get<1>(projV1), std::get<0>(projV2), std::get<1>(projV2), red);
+        img.DrawLine(std::get<0>(projV1), std::get<1>(projV1), std::get<0>(projV3), std::get<1>(projV3), red);
+        img.DrawLine(std::get<0>(projV2), std::get<1>(projV2), std::get<0>(projV3), std::get<1>(projV3), red);
     }
 
     img.WriteTGAFile("diablo.tga");
