bc63f48cc830a3cfe30d3ce2c1e7c1a401c7a78d
jonathan <jonathhu@stud.ntnu.no>
2026-07-25 14:53:37 +0200

    Draw triangle with simple parametric approach

--- main.cpp
+++ main.cpp
@@ -5,6 +5,19 @@ constexpr Pixel red = {0, 0, 255};
 constexpr Pixel green = {0, 255, 0};
 constexpr Pixel blue = {255, 0, 0};
 
+void DrawLine(Image& img, Pixel color, uint8_t ax, uint8_t ay, uint8_t bx, uint8_t by) {
+    float t = 0;
+
+    while (t <= 1.0f) {
+        uint8_t x = ax + t * (bx - ax);
+        uint8_t y = ay + t * (by - ay);
+
+        img.SetPixel(x, y, color);
+
+        t += 0.0001;
+    }
+}
+
 int main() {
     constexpr uint16_t width = 64;
     constexpr uint16_t height = 64;
@@ -15,9 +28,9 @@ int main() {
     uint8_t bx = 12, by = 37;
     uint8_t cx = 62, cy = 53;
 
-    img.SetPixel(ax, ay, white);
-    img.SetPixel(bx, by, white);
-    img.SetPixel(cx, cy, white);
+    DrawLine(img, blue, ax, ay, bx, by);
+    DrawLine(img, red, ax, ay, cx, cy);
+    DrawLine(img, green, bx, by, cx, cy);
 
     img.WriteTGAFile("img.tga");
     return 0;
