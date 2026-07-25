8774215276d1ed5822fe8a341dc058a39718fe5c
jonathan <jonathhu@stud.ntnu.no>
2026-07-25 16:35:52 +0200

    Parse vertices from model file

--- Mesh.cpp
+++ Mesh.cpp
@@ -1,5 +1,31 @@
-//
-// Created by Jonathan Hübertz on 25/07/2026.
-//
-
 #include "Mesh.hpp"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+Mesh::Mesh(const char* path) {
+    std::ifstream inputFile(path);
+
+    if (!inputFile.is_open()) {
+        std::cerr << "Failed to open file: " << path << std::endl;
+    }
+
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        if (line.empty()) {
+            continue;
+        }
+
+        std::stringstream ss(line);
+        std::string prefix;
+        ss >> prefix;
+
+        if (prefix == "v") {
+            Vec3 vert;
+            ss >> vert.x >> vert.y >> vert.z;
+            vertices.push_back(vert);
+        }
+    }
+
+    std::cout << "Num vertices: " << vertices.size() << std::endl;
+};
