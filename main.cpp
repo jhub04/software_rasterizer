25ace8bedb71166904a39d35f2498d20eecc0f87
jonathan <jonathhu@stud.ntnu.no>
2026-07-25 17:59:22 +0200

    Draw projected vertices as pixels

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "Image.hpp"
 #include "Mesh.hpp"
+#include <tuple>
 #include <iostream>
 
 constexpr Pixel white = {255, 255, 255};
@@ -8,8 +9,8 @@ constexpr Pixel green = {0, 255, 0};
 constexpr Pixel blue = {255, 0, 0};
 
 int main(int argc, char* argv[]) {
-    constexpr uint16_t width = 64;
-    constexpr uint16_t height = 64;
+    constexpr uint16_t width = 256;
+    constexpr uint16_t height = 256;
 
     if (argc != 2) {
         std::cerr << "Only one argument is supported!" << std::endl;
@@ -19,6 +20,15 @@ int main(int argc, char* argv[]) {
     const char* inputPath = argv[1];
 
     Mesh mesh( inputPath);
+
+    Image img(width, height);
+
+    for (Vec3& v : mesh.GetVertices()) {
+        std::tuple proj = img.ProjectTo2D(v);
+        img.SetPixel(std::get<0>(proj), std::get<1>(proj), red);
+    }
+
+    img.WriteTGAFile("diablo.tga");
 
     return 0;
 }
